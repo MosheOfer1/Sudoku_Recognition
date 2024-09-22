27e003bdf5f19544b2884bf7ae83569342667330
Moshe Ofer <mosheofer1@gmail.com>
2024-09-23 00:30:23 +0300

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -22,8 +22,6 @@ class DigitClassifier(nn.Module):
         self.bn3 = nn.BatchNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(512)
-        self.conv5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
-        self.bn5 = nn.BatchNorm2d(512)
 
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(512 * 4 * 4, 1024)  # Increased to 1024
@@ -37,7 +35,6 @@ class DigitClassifier(nn.Module):
         x = self.pool(self.bn2(self.relu(self.conv2(x))))
         x = self.pool(self.bn3(self.relu(self.conv3(x))))
         x = self.pool(self.bn4(self.relu(self.conv4(x))))  # Added conv4 block
-        x = self.pool(self.bn5(self.relu(self.conv5(x))))  # Added conv5 block
         x = x.view(-1, 512 * 4 * 4)  # Adjust for new input size
         x = self.dropout(self.relu(self.fc1(x)))
         x = self.dropout(self.relu(self.fc2(x)))
