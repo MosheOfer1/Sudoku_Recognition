11acf0d04a65f7b143016a3b00888611b4c0b929
Moshe Ofer <mosheofer1@gmail.com>
2024-09-30 10:44:09 +0300

    Update grid_recognition.py

--- src/grid_recognition.py
+++ src/grid_recognition.py
@@ -236,7 +236,7 @@ def extract_sudoku_grid_and_classify(warped_image, bw_warped_image, model, devic
 
     # After processing all the cells, plot all the predictions in a single figure
     # plot_all_digit_predictions(predicted_cells, [x[1] for x in probs_list])
-    limit = 80
+    limit = 400
     # Generate the most probable Sudoku configuration and attempt to solve it
     for i, sudoku_grid in enumerate(generate_most_probable_configuration(probs_list)):
         if i > limit:
