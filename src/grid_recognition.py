f7be3666eb126d0b6fa60296710958f59ec24042
Moshe Ofer <mosheofer1@gmail.com>
2024-09-30 10:41:37 +0300

    stop cond

--- src/grid_recognition.py
+++ src/grid_recognition.py
@@ -235,16 +235,21 @@ def extract_sudoku_grid_and_classify(warped_image, bw_warped_image, model, devic
             #     print(f"Removed the {index} image with prob: {max_prob}, entropy: {entropy}")
 
     # After processing all the cells, plot all the predictions in a single figure
-    # plot_all_digit_predictions(predicted_cells, [x[1] for x in probs_list])
-
+    plot_all_digit_predictions(predicted_cells, [x[1] for x in probs_list])
+    limit = 80
     # Generate the most probable Sudoku configuration and attempt to solve it
     for i, sudoku_grid in enumerate(generate_most_probable_configuration(probs_list)):
-        if not is_valid_sudoku(sudoku_grid):
-            print("Not a valid grid")
-            continue
+        if i > limit:
+            return None, "Operation was stopped."
+
         if stop_event is not None:
             if stop_event.is_set():
                 return None, "Operation was stopped."
+
+        if not is_valid_sudoku(sudoku_grid):
+            print("Not a valid grid")
+            continue
+
         # Use the solve_sudoku_copy to solve without modifying the original grid
         solution = solve_sudoku_copy(sudoku_grid)
         print(solution)
